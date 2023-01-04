929d0250627b9917bc380912b5487eecb1d90da7
wSwanepoel199 <wjaco.swanepoel@gmail.com>
2023-01-04 14:55:14 +1100

    added dynamic origin for cors config

--- bin/cors.js
+++ bin/cors.js
@@ -1,6 +1,15 @@
+const createError = require("http-errors");
+
+const whitelist = [/192\.168\./, 'https://paritysl-frontend.onrender.com/'];
 
 const cors = {
-  origin: [/192\.168\./, 'https://paritysl-frontend.onrender.com/'],
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, ture);
+    } else {
+      callback(createError.Unauthorized("Blocked By Cors"));
+    }
+  },
   methods: ['GET', 'POST', 'PUT'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
